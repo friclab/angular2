4129d66ecb0338532dad724338e078a2f4da206a
s.coreggioli <sc@mobyt.it>
2017-03-28 14:03:10 +0200

    registration form: automaticoreactive 2

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -11,23 +11,23 @@ export class RegistrationComponent implements OnInit {
 
   constructor() {
 
-
   }
 
   onSubmit($event) {
-    console.log(this.registrationForm);
+    console.log(this.registrationForm.valid, this.registrationForm.value);
   }
 
   ngOnInit() {
     this.registrationForm = new FormGroup({
-      name: new FormControl(),
-      lastname: new FormControl(),
-      email: new FormControl(),
-      password: new FormControl(),
-      phone: new FormControl(),
-      repeatedPassword: new FormControl(),
-      privacyTerms: new FormControl()
+      name: new FormControl("", Validators.required),
+      lastname: new FormControl("", Validators.minLength(3)),
+      email: new FormControl("", Validators.email),
+      password: new FormControl(""),
+      phone: new FormControl(""),
+      repeatedPassword: new FormControl(""),
+      privacyTerms: new FormControl(false,[Validators.requiredTrue])
     });
   }
 
 }
+ 
